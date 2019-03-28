c3fa431aca08d5b31ec78df5baa67c0992959bd4
DESKTOP-1TO9HQ1\Bertram <bertramf@live.nl>
2019-03-28 08:05:10 +0100

    new grass anim

--- Assets/_Scripts/Grass.cs
+++ Assets/_Scripts/Grass.cs
@@ -12,7 +12,6 @@ public class Grass : MonoBehaviour{
 
     private void Start() {
         anim_c = GetComponent<Animator>();
-        idleAnim = anim_c.state
 
         //Set random xScale
         float xScale = 1;
@@ -28,15 +27,15 @@ public class Grass : MonoBehaviour{
         //Set random idleTime
         float randomStartTime = Random.Range(0f, 0.5f);
 
-        //Set random idleAnimation
-        int randomIdle = Random.RandomRange(0, 2);
-        if(randomIdle == 1) {
-            idleAnim.AddClip(clip1, "beuuh");
-        }
-        else {
-            idleAnim.AddClip(clip1, "mwaah");
-        }
-        
+        ////set random idleanimation
+        //int randomidle = random.randomrange(0, 2);
+        //if (randomidle == 1) {
+        //    idleanim.addclip(clip1, "beuuh");
+        //}
+        //else {
+        //    idleanim.addclip(clip1, "mwaah");
+        //}
+
         Invoke("StartIdleAnimation", randomStartTime);
     }
 
