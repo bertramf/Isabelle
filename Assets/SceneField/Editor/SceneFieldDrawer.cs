f1f43a326d00262ae54754728d333d7e2d90e394
DESKTOP-1TO9HQ1\Bertram <bertramf@live.nl>
2019-05-22 11:33:35 +0200

    - Added scenefield folder + scripts

---
+++ Assets/SceneField/Editor/SceneFieldDrawer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[CustomPropertyDrawer(typeof(SceneField))]
+public class SceneFieldDrawer : PropertyDrawer {
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+		EditorGUI.BeginProperty(position, GUIContent.none, property);
+
+		SerializedProperty sceneProperty = property.FindPropertyRelative("scene");
+		SerializedProperty nameProperty = property.FindPropertyRelative("name");
+		position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+		if(sceneProperty != null) {
+			EditorGUI.BeginChangeCheck();
+			Object sceneValue = EditorGUI.ObjectField(position, sceneProperty.objectReferenceValue, typeof(SceneAsset), false);
+			if (EditorGUI.EndChangeCheck()) {
+				sceneProperty.objectReferenceValue = sceneValue;
+				if(sceneValue != null) {
+					string scenePath = AssetDatabase.GetAssetPath(sceneValue);
+					int assetsIndex = scenePath.IndexOf("Assets", StringComparison.Ordinal) + 7;
+					int extensionIndex = scenePath.LastIndexOf(".unity", StringComparison.Ordinal);
+					nameProperty.stringValue = scenePath.Substring(assetsIndex, extensionIndex - assetsIndex);
+				}
+			}
+		}
+
+		EditorGUI.EndProperty();
+	}
+
+}
---
+++ Assets/SceneField/SceneField.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[Serializable]
+public class SceneField {
+
+	public string SceneName { get { return name; } }
+
+	[SerializeField] private string name;
+#if UNITY_EDITOR
+	[SerializeField] private Object scene;
+#endif
+
+	public static implicit operator string(SceneField sceneField) {
+		return sceneField.name;
+	}
+
+}
