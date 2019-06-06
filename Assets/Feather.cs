b5ae889890654b7e1510171e01895a546ca4b2da
DESKTOP-1TO9HQ1\Bertram <bertramf@live.nl>
2019-06-07 00:50:01 +0200

    - new ivy 2 stuff - scene chapter1_01 made!

---
+++ Assets/Feather.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feather : MonoBehaviour{
+
+    private bool isInTrigger;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag != "Player") {
+            return;
+        }
+        isInTrigger = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag != "Player") {
+            return;
+        }
+        isInTrigger = false;
+    }
+
+    private void Update() {
+        if (isInTrigger) {
+            if (Input.GetButtonDown("Y")) {
+                DestroyFeather();
+            }
+        }
+    }
+
+    private void DestroyFeather() {
+        Destroy(this.gameObject);
+    }
+
+}
